2aa1deffc50a4bbc1013fa3adbd21efd408dcfde
frantorres <torres_francois@outlook.com>
2025-09-19 06:47:15 -0300

    Atualiza socket.io para usar backend público

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
 // 🔹 SOCKET.IO
-const socket = io('http://34.39.159.103:3000');
+const socket = io('http://34.39.159.103:3000', { transports: ['websocket', 'polling'] });
 socket.on('connect', () => console.log('✅ Conectado ao backend'));
 
 // -----------------------------
